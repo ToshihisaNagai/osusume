1e1e796d994f1f4a61d38d2c81c019ca05ae40e2
“ToshihisaNagai” <toshihisa.nagai@gmail.com>
2022-07-13 04:39:27 +0000

    16 password change

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+  Route::get('users/mypage', 'UserController@mypage')->name('mypage');
+  Route::get('users/mypage/edit', 'UserController@edit')->name('mypage.edit');
+  Route::get('users/mypage/address/edit', 'UserController@edit_address')->name('mypage.edit_address');
+  Route::put('users/mypage', 'UserController@update')->name('mypage.update');
+　Route::get('users/mypage/password/edit', 'UserController@edit_password')->name('mypage.edit_password');
+  Route::put('users/mypage/password', 'UserController@update_password')->name('mypage.update_password');
+  
 Route::post('products/{product}/reviews', 'ReviewController@store');
 
+Route::get('products/{product}/favorite', 'ProductController@favorite')->name('products.favorite');
 Route::resource('products', 'ProductController');
 Auth::routes(['verify' => true]);
 
